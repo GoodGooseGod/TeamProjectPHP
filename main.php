dc57aa0136c1075c17a154d5bff8f2e7c2ed7cee
PoorJew56k <ragimovemin9@gmail.com>
2025-06-07 15:40:56 +0700

    Update main.php

--- main.php
+++ main.php
@@ -5,7 +5,18 @@
         $orders[] = $str;
         $str = readline();
     }
-    foreach ($orders as $i){
-        echo $i . "\n";
+    
+    $orderNumber = 1;
+    foreach ($orders as $order) {
+        list($dishes, $total) = explode(' ', $order);
+        $dishes = (int)$dishes;
+        $total = (int)$total;
+        
+        if ($dishes % 2 == 0 && $total >= 1000) {
+            echo "Заказ $orderNumber: скидка\n";
+        } else {
+            echo "Заказ $orderNumber: без скидки\n";
+        }
+        $orderNumber++;
     }
 ?>
