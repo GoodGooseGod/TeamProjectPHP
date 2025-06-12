7c49dc49d20664bf2fe27b7dde04d9e54bfff534
Shemelov Anton <shemelov03@mail.ru>
2025-06-13 02:20:02 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.php

--- process.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $owner = $_POST['Владелец'];
-    $size = $_POST['Пенис'];
-
-    echo "Владелец: " . htmlspecialchars($owner) . "<br>";
-    echo "Пенис: " . htmlspecialchars($size) . " см<br>";
-}
-?>
