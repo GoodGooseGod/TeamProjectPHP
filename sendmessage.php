4ebc0bdf23e3fc8a85d37249fd85d2aa3d2a95a4
Shemelov Anton <shemelov03@mail.ru>
2025-06-13 22:58:56 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.php

--- sendmessage.php
+++ sendmessage.php
@@ -2,12 +2,15 @@
 $separator = " блять это лучший разделитель я тебе отвечаю ";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
-    $from = $_SESSION['login'];
+    $from = $_COOKIE['login'];
     $to = htmlspecialchars($_POST['Name']);
     $text = htmlspecialchars($_POST['Text']);
 
     $file = fopen("messages.txt", "a");
     fwrite($file, $from . $separator . $to . $separator . $text . "\n");
     fclose($file);
+
+    header("Location: welcome.php");
+    exit;
 }
 ?>
