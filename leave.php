d420339f2767feb0d6be8f2aa6ce0067856e5a5f
Shemelov Anton <shemelov03@mail.ru>
2025-06-22 14:33:39 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.php

--- leave.php
+++ leave.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 session_destroy();
-setcookie('username', '', time() - 3600, "/"); // Удаление куки
+setcookie('login', '', time() - 3600, "/"); // Удаление куки
 header("Location: main.php");
 exit;
 ?>
